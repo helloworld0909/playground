bbe6c4ee1e9f7ce2b8b69934338761563eba1286
Zheng Luo <helloworld09@163.com>
2018-08-18 20:39:35 +0800

    Pass test3

--- data-management-pj1/rbf/pfm.cc
+++ data-management-pj1/rbf/pfm.cc
@@ -2,63 +2,106 @@
 #include <fstream>
 #include <iostream>
 #include <cstdio>
+#include <sys/stat.h>
 
 using namespace std;
 
-const string DATA_PATH = "";
+PagedFileManager *PagedFileManager::_pf_manager = 0;
 
-PagedFileManager* PagedFileManager::_pf_manager = 0;
-
-PagedFileManager* PagedFileManager::instance()
+PagedFileManager *PagedFileManager::instance()
 {
-    if(!_pf_manager)
+    if (!_pf_manager)
         _pf_manager = new PagedFileManager();
 
     return _pf_manager;
 }
 
-
 PagedFileManager::PagedFileManager()
 {
 }
-
 
 PagedFileManager::~PagedFileManager()
 {
 }
 
+bool isFileExists(const string &fileName)
+{
+    struct stat stFileInfo;
+    return stat(fileName.c_str(), &stFileInfo) == 0;
+}
 
 RC PagedFileManager::createFile(const string &fileName)
 {
-    fstream page(DATA_PATH + fileName, ios::in);
-    if (page) {
-        return -1;
-    } else {
-        page.close();
-        ofstream page(DATA_PATH + fileName);
-        page.close();
-        return 0;
+    if (isFileExists(fileName))
+    {
+        return FAIL;
     }
-}
 
+    ofstream file(DATA_PATH + fileName, ios::binary);
+    byte header[PAGE_SIZE] = {IDENTIFIER};
+    file.write(header, PAGE_SIZE);
+    file.close();
+    return SUCCESS;
+}
 
 RC PagedFileManager::destroyFile(const string &fileName)
 {
-    return remove(fileName.c_str());
+    int rc = remove(fileName.c_str());
+    if (rc == 0)
+    {
+        return SUCCESS;
+    }
+    else
+    {
+        return FAIL;
+    }
 }
-
 
 RC PagedFileManager::openFile(const string &fileName, FileHandle &fileHandle)
 {
-    return -1;
-}
+    if (fileHandle.file.is_open() || !isFileExists(fileName))
+    {
+        return FAIL;
+    }
 
+    fileHandle.file.open(fileName, ios::in | ios::out | ios::binary);
+    if (!fileHandle.file)
+    {
+        return FAIL;
+    }
+
+    byte header[PAGE_SIZE];
+    fileHandle.file.read(header, PAGE_SIZE);
+
+    if (header[0] != IDENTIFIER)
+    {
+        return FAIL;
+    }
+
+    fileHandle.numOfPages = header[1] | header[2] << 8;
+    fileHandle.readPageCounter = header[3] | header[4] << 8;
+    fileHandle.writePageCounter = header[5] | header[6] << 8;
+    fileHandle.appendPageCounter = header[7] | header[8] << 8;
+
+    return SUCCESS;
+}
 
 RC PagedFileManager::closeFile(FileHandle &fileHandle)
 {
-    return -1;
-}
+    if (!fileHandle.file.is_open())
+    {
+        return FAIL;
+    }
+    fileHandle.file.seekp(sizeof(IDENTIFIER), ios::beg);
 
+    fileHandle.file.write((char *)&fileHandle.numOfPages, sizeof(unsigned));
+    fileHandle.file.write((char *)&fileHandle.readPageCounter, sizeof(unsigned));
+    fileHandle.file.write((char *)&fileHandle.writePageCounter, sizeof(unsigned));
+    fileHandle.file.write((char *)&fileHandle.appendPageCounter, sizeof(unsigned));
+
+    fileHandle.file.close();
+    return SUCCESS;
+}
 
 FileHandle::FileHandle()
 {
@@ -67,35 +110,29 @@ FileHandle::FileHandle()
     appendPageCounter = 0;
 }
 
-
 FileHandle::~FileHandle()
 {
 }
-
 
 RC FileHandle::readPage(PageNum pageNum, void *data)
 {
     return -1;
 }
 
-
 RC FileHandle::writePage(PageNum pageNum, const void *data)
 {
     return -1;
 }
-
 
 RC FileHandle::appendPage(const void *data)
 {
     return -1;
 }
 
-
 unsigned FileHandle::getNumberOfPages()
 {
-    return -1;
+    return numOfPages;
 }
-
 
 RC FileHandle::collectCounterValues(unsigned &readPageCount, unsigned &writePageCount, unsigned &appendPageCount)
 {
--- data-management-pj1/rbf/pfm.h
+++ data-management-pj1/rbf/pfm.h
@@ -6,47 +6,54 @@ typedef int RC;
 typedef char byte;
 
 #define PAGE_SIZE 4096
+#define SUCCESS 0
+#define FAIL -1
 #include <string>
 #include <climits>
+#include <fstream>
 using namespace std;
 
+const string DATA_PATH = "";
+const byte IDENTIFIER = 0x88;
 class FileHandle;
 
 class PagedFileManager
 {
-public:
-    static PagedFileManager* instance();                                  // Access to the _pf_manager instance
+  public:
+    static PagedFileManager *instance(); // Access to the _pf_manager instance
 
-    RC createFile    (const string &fileName);                            // Create a new file
-    RC destroyFile   (const string &fileName);                            // Destroy a file
-    RC openFile      (const string &fileName, FileHandle &fileHandle);    // Open a file
-    RC closeFile     (FileHandle &fileHandle);                            // Close a file
+    RC createFile(const string &fileName);                       // Create a new file
+    RC destroyFile(const string &fileName);                      // Destroy a file
+    RC openFile(const string &fileName, FileHandle &fileHandle); // Open a file
+    RC closeFile(FileHandle &fileHandle);                        // Close a file
 
-protected:
-    PagedFileManager();                                                   // Constructor
-    ~PagedFileManager();                                                  // Destructor
+  protected:
+    PagedFileManager();  // Constructor
+    ~PagedFileManager(); // Destructor
 
-private:
+  private:
     static PagedFileManager *_pf_manager;
 };
 
-
 class FileHandle
 {
-public:
+  public:
     // variables to keep the counter for each operation
     unsigned readPageCounter;
     unsigned writePageCounter;
     unsigned appendPageCounter;
-    
-    FileHandle();                                                         // Default constructor
-    ~FileHandle();                                                        // Destructor
 
-    RC readPage(PageNum pageNum, void *data);                             // Get a specific page
-    RC writePage(PageNum pageNum, const void *data);                      // Write a specific page
-    RC appendPage(const void *data);                                      // Append a specific page
-    unsigned getNumberOfPages();                                          // Get the number of pages in the file
-    RC collectCounterValues(unsigned &readPageCount, unsigned &writePageCount, unsigned &appendPageCount);  // Put the current counter values into variables
-}; 
+    FileHandle();  // Default constructor
+    ~FileHandle(); // Destructor
+
+    RC readPage(PageNum pageNum, void *data);                                                              // Get a specific page
+    RC writePage(PageNum pageNum, const void *data);                                                       // Write a specific page
+    RC appendPage(const void *data);                                                                       // Append a specific page
+    unsigned getNumberOfPages();                                                                           // Get the number of pages in the file
+    RC collectCounterValues(unsigned &readPageCount, unsigned &writePageCount, unsigned &appendPageCount); // Put the current counter values into variables
+
+    fstream file;
+    unsigned numOfPages = 0;
+};
 
 #endif
